c3819bae7e1b983c3810a4a851143561b5f6c578
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-12-07 00:02:42 +0100

    replace AbundanceTable with ComMatrix

--- src/abundances.jl
+++ src/abundances.jl
@@ -1,26 +1,10 @@
 # Methods for absolute and relative abundances
 
-struct AbundanceTable{T<:Real} <: AbstractArray{T,2}
-    table::Array{T,2}
-    samples::Vector{S} where S
-    features::Vector{R} where R
-end
-
-function AbundanceTable(df::DataFrame)
-    return AbundanceTable(
-            Matrix(df[:,2:end]),
-            names(df[2:end]),
-            Vector(df[1]))
-end
-function AbundanceTable(table::Array{T,2}) where T<:Real
-    return AbundanceTable(
-             table,
-             Vector{Int64}(1:size(table,2)),
-             Vector{Int64}(1:size(table,1)))
-end
+abundancetable(df::DataFrame) = ComMatrix(df, sitecolumns = true)
+abundancetable(table::Array{T,2}) where T<:Real = ComMatrix(table')
 
 @forward_func AbundanceTable.table Base.getindex, Base.setindex, Base.length, Base.size
-
+#do something about these forward funcions
 
 """
 Filter an abundance table to the top `n` species accross all samples
