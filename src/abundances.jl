035ee8bdaf3105d1dc4535efdd90dd0521cb39be
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-12-07 10:17:33 +0100

    rewrite filterabund

--- src/abundances.jl
+++ src/abundances.jl
@@ -11,20 +11,14 @@ Filter an abundance table to the top `n` species accross all samples
 
 This function also adds a row for "other", which sums the
 """
-function filterabund(abun::AbundanceTable, n::Int=10)
+function filterabund(abun::AbstractComMatrix, n::Int=10)
+    srt = sortperm(total_abundance_species(abun), rev=true)
+    incl, compact = srt[1:n], srt[n+1:end]
     # TODO: add prevalence filter
 
-    totals = [sum(abun[i,:]) for i in 1:size(abun, 1)]
-
-    srt = sortperm(totals, rev=true)
-
-    newabun = abun[srt[1:n], :]
-
-    remainder = [sum(abun[srt[n+1:end], i]) for i in 1:size(abun, 2)]'
-    newabun = vcat(newabun, remainder)
-    newrows = cat(1, abun.features[srt[1:n]], ["other"])
-
-    return AbundanceTable(newabun, abun.samples, newrows)
+    newabun = view(abun, species = incl)
+    remainder = total_abundance_site(view(abun, species = compact))
+    ComMatrix(hcat(newabun.occurrences, remainder), [specnames(newabun); "other"] , sitenames(abun))
 end
 
 filterabund(df::DataFrame, n::Int=10) = filterabund(AbundanceTable(df), n)
