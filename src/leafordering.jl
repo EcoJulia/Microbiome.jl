6b6b6f2a4fa248ef5be8dd9c5450a72382cfec92
Kevin Bonham <kevbonham@gmail.com>
2017-12-13 14:14:06 -0500

    progress on leafordering

--- src/leafordering.jl
+++ src/leafordering.jl
@@ -2,33 +2,68 @@
 Based on:
 Bar-Joseph et. al. "Fast optimal leaf ordering for hierarchical clustering." _Bioinformatics_. (2001)
 =#
+using Distances
 
-leaves = [1,2,3,4,5]
 
-dm = [0. .1 .4 .5 .6;
-      .1 0. .45 .3 .55;
-      .4 .45 0. .2 .7;
-      .5 .3 .2 0. .8;
-      .6 .55 .7 .8 0.]
+m = rand(5, 15)
+dm = pairwise(BrayCurtis(), m)
 
-current_max = -Inf64
 hcl = hclust(dm, :average)
+dump(hcl)
 
+function optimalorder(hc::Hclust, dm::Array{Float64,2})
+    ord = hc.order
+    extents = Tuple[]
 
-orders = Tuple[]
-connects = Tuple[]
+    for (vl, vr) in zip(hc.merge[:,1], hc.merge[:,2])
+        @show vl, vr
+        if vl < 0 && vr < 0
+            m = findfirst(abs(vl) .== ord)
+            k = findfirst(abs(vr) .== ord)
+            push!(extents, (m, k))
+        elseif vl < 0
+            m = findfirst(abs(vl) .== ord)
+            kidx = extents[vr][1]
+            widx = extents[vr][2]
 
-merge = [(hcl.merge[i,1], hcl.merge[i,2]) for i in 1:size(hcl.merge, 1)]
-for (vl, vr) in merge
-    if vl < 0 && vr < 0
-        push!(orders, (abs(vl), abs(vr)))
-    elseif vl < 0
-        push!(orders, (abs(vl),))
-    elseif vr < 0
-        push!(orders, (abs(vr),))
-    else
-        push!(connects, (vl, vr))
+            k = ord[kidx]
+            w = ord[widx]
+            flp = flip1(m, k, w, dm)
+            flp == 2 && reverse!(ord, kidx, widx)
+            push!(extents, (m, widx))
+        elseif vl > 0 && vr > 0
+            uidx = extents[vl][1]
+            midx = extents[vl][2]
+            kidx = extents[vr][1]
+            widx = extents[vr][2]
+
+            u = ord[uidx]
+            m = ord[midx]
+            k = ord[kidx]
+            w = ord[widx]
+
+            flp = flip2(u, m, k, w, dm)
+            @show flp
+            (flp == 2 || flp == 4) && reverse!(ord, uidx, midx)
+            (flp == 3 || flp == 4) && reverse!(ord, kidx, widx)
+            push!(extents, (uidx, widx))
+
+        else
+            error("ooops")
+        end
     end
+    return ord
+end
+
+hcl.order = optimalorder(hcl, dm)
+
+"""
+For 1 multi-leaf branch and a leaf, determine if flipping branch is required
+1 = do not flip
+2 = flip right
+"""
+function flip1(m::Int, k::Int, w::Int, dm::Array{Float64,2})
+    @inbounds dm[m,k] <= dm[m,w] ? 1 : 2
 end
 
 """
@@ -38,17 +73,7 @@ For 2 multi-leaf branches, determine if one or two flips is required
 3 = flip right
 4 = flip both
 """
-function flip2(order, dm::Array{Float64,2}, idx1::Int64, idx2::Int64)
-    u = order[idx1][1]
-    m = order[idx1][end]
-    k = order[idx2][1]
-    w = order[idx2][end]
-
-    ds = [dm[m,k], dm[u,k], dm[m,w], dm[u,w]]
-    return sortperm(ds)[1]
+function flip2(u::Int, m::Int, k::Int, w::Int, dm::Array{Float64,2})
+    s = sortperm([dm[m,k], dm[u,k], dm[m,w], dm[u,w]])
+    return s[1]
 end
-
-flip2(orders, dm, 1, 2)
-
-connects
-orders
