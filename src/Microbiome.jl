ef5582b4755ea10436c8b82c81f0caefd02044f1
Kevin Bonham <kevbonham@gmail.com>
2017-12-14 11:53:49 -0500

    Refactor leafordering

--- src/Microbiome.jl
+++ src/Microbiome.jl
@@ -32,16 +32,19 @@ using StatPlots
 using StatsBase
 using Distances
 using Colors
+using Clustering
+
 
 import DataFrames: DataFrame
-import Clustering: Hclust, hclust
 import Base: getindex, setindex, length
 
 
 include("utils.jl")
 include("abundances.jl")
 include("similarity.jl")
+include("leafordering.jl")
 include("plotting.jl")
 include("biobakery_utils.jl")
+
 
 end  # module Microbiome
--- src/leafordering.jl
+++ src/leafordering.jl
@@ -14,50 +14,60 @@ Based on:
 Bar-Joseph et. al. "Fast optimal leaf ordering for hierarchical clustering." _Bioinformatics_. (2001)
 """
 function optimalorder(hc::Hclust, dm::Array{Float64,2})
-    ord = hc.order
-    extents = Tuple[]
-
-    for (vl, vr) in zip(hc.merge[:,1], hc.merge[:,2])
-        if vl < 0 && vr < 0
-            m = findfirst(abs(vl) .== ord)
-            k = findfirst(abs(vr) .== ord)
-            push!(extents, (m, k))
-        elseif vl < 0
-            m = findfirst(abs(vl) .== ord)
-            kidx = extents[vr][1]
-            widx = extents[vr][2]
-
-            k = ord[kidx]
-            w = ord[widx]
-            flp = flip1(m, k, w, dm)
-            flp == 2 && reverse!(ord, kidx, widx)
-            push!(extents, (m, widx))
-        elseif vl > 0 && vr > 0
-            uidx = extents[vl][1]
-            midx = extents[vl][2]
-            kidx = extents[vr][1]
-            widx = extents[vr][2]
-
-            u = ord[uidx]
-            m = ord[midx]
-            k = ord[kidx]
-            w = ord[widx]
-
-            flp = flip2(u, m, k, w, dm)
-            (flp == 2 || flp == 4) && reverse!(ord, uidx, midx)
-            (flp == 3 || flp == 4) && reverse!(ord, kidx, widx)
-            push!(extents, (uidx, widx))
-
-        else
-            error("ooops")
-        end
-    end
+    ord = copy(hc.order)
+    ext = Tuple[]
+    orderleaves!(ord, ext, hc, dm)
     return ord
 end
 
+
 function optimalorder!(hc::Hclust, dm::Array{Float64,2})
-    hc.order = optimalorder(hc, dm)
+    ord = hc.order
+    ext = Tuple[]
+    orderleaves!(ord, ext, hc, dm)
 end
+
+
+function orderleaves!(order::Vector{Int}, extents::Vector{Tuple}, hcl::Hclust, dm::Array{Float64,2})
+    for (vl, vr) in zip(hcl.merge[:,1], hcl.merge[:,2])
+        (u, m, uidx, midx) = leaflocs(vl, order, extents)
+        (w, k, widx, kidx) = leaflocs(vr, order, extents)
+        if vl < 0 && vr < 0
+            push!(extents, (m, k))
+        elseif vl < 0
+            flp = flip1(m, k, w, dm)
+            flp == 2 && reverse!(order, widx, kidx)
+            push!(extents, (m, widx))
+        elseif vl > 0 && vr > 0
+            flp = flip2(u, m, k, w, dm)
+            (flp == 2 || flp == 4) && reverse!(order, uidx, midx)
+            (flp == 3 || flp == 4) && reverse!(order, widx, uidx)
+            push!(extents, (uidx, widx))
+        else
+            error("invalid 'merge' order in Hclust: ($vl, $vr) ")
+        end
+    end
+end
+
+
+function leaflocs(v::Int, order::Vector{Int}, extents::Vector{Tuple})
+    if v < 0
+        outer = 0
+        inner = findfirst(abs(v) .== order)
+        branchleft = 0
+        branchright = 0
+    elseif v > 0
+        branchleft = extents[abs(v)][1]
+        branchright = extents[abs(v)][2]
+        outer = order[branchleft]
+        inner = order[branchright]
+    else
+        error("leaf postition cannot be zero")
+    end
+    return outer, inner, branchleft, branchright
+end
+
+
 
 """
 For 1 multi-leaf branch and a leaf, determine if flipping branch is required
@@ -79,13 +89,3 @@ function flip2(u::Int, m::Int, k::Int, w::Int, dm::Array{Float64,2})
     s = sortperm([dm[m,k], dm[u,k], dm[m,w], dm[u,w]])
     return s[1]
 end
-
-
-m = rand(1000, 5000)
-dm = pairwise(BrayCurtis(), m)
-
-using BenchmarkTools
-
-@benchmark hcl = hclust(dm, :single)
-
-@benchmark optimalorder(hcl, dm)
