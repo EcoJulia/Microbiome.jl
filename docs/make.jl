082c1fdadb7a6936383a42e56e376268af3622a5
Ben J. Ward <ward9250@gmail.com>
2018-02-01 15:39:44 +0000

    Update make.jl
    @kescobo This should fix the Documenter problem you were having :)

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,16 @@
 using Documenter, Microbiome
 
-makedocs()
+makedocs(
+    format = :html,
+    sitename = "Microbiome.jl",
+    pages = [
+        "Home" => "index.md",
+        "Microbial Abundances" => "abundances.md",
+        "Distances & Dissimilarity" => "similarities.md",
+        "Contributing" => "contributing.md"
+    ],
+    authors = "Kevin Bonham."
+)
 
 deploydocs(
     repo = "github.com/BioJulia/Microbiome.jl.git",
