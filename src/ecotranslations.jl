d8c1c68690a935a7115d3de71368dace08b594cb
Kevin Bonham <kevbonham@gmail.com>
2018-03-23 09:30:14 -0400

    Last fixes

--- src/ecotranslations.jl
+++ src/ecotranslations.jl
@@ -1,13 +1,13 @@
 # Getters and setters with microbiome-flavored names
 
 # species -> features
-const nfeatures(com::AbstractComMatrix) = nspecies(com)
-const getfeature(com::AbstractComMatrix, idx) = getspecies(com, idx)
-const featurenames(com::AbstractComMatrix) = specnames(com)
-const featuretotals(com::AbstractComMatrix) = speciestotals(com)
+const nfeatures = nspecies
+const getfeature = getspecies
+const featurenames = specnames
+const featuretotals = speciestotals
 
 # sites -> samples
-const nsamples(com::AbstractComMatrix) = nsites(com)
-const getsample(com::AbstractComMatrix, idx) = getsite(com, idx)
-const samplenames(com::AbstractComMatrix) = sitenames(com)
-const sampletotals(com::AbstractComMatrix) = sitetotals(com)
+const nsamples = nsites
+const getsample = getsite
+const samplenames = sitenames
+const sampletotals = sitetotals
