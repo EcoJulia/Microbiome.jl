c6cab1c861ba9214a8ba8038c0bd257948d48cad
Annelle Abatoni Kayisire <aa1@wellesley.edu>
2021-09-27 15:05:36 -0400

    Correcting insert! doc for CP & MS

--- src/profiles.jl
+++ src/profiles.jl
@@ -563,7 +563,7 @@ end
 
 Insert a value `val` to the metadata of `sample` in a CommunityProfile `commp` using a Symbol `prop`, 
 and it will throw an error if `prop` exists. 
-If you don't want an error to be thrown if the value does not exist, use [`set!`](@ref).
+If you don't want an error to be thrown if the value exists, use [`set!`](@ref).
 
 
 Examples
--- src/samples_features.jl
+++ src/samples_features.jl
@@ -98,7 +98,7 @@ end
 
 Insert a value `val` to the metadata of sample `as` using a Symbol `prop`, 
 and it will throw an error if `prop` exists. 
-If you don't want an error to be thrown if the value does not exist, use [`set!`](@ref).
+If you don't want an error to be thrown if the value exists, use [`set!`](@ref).
 
 
 Examples
