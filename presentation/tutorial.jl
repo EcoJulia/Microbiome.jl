4fa87134dd087acea2731785be880188e2617bd5
Anika Luo <al8@wellesley.edu>
2022-06-22 15:38:35 -0400

    create tutorial file

---
+++ presentation/tutorial.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ bd9652fc-f262-11ec-3cf6-71de5b26f0f0
+
+
+# ╔═╡ Cell order:
+# ╠═bd9652fc-f262-11ec-3cf6-71de5b26f0f0
