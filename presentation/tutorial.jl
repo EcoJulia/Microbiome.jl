9cd048f51357b2a014cfccaf194161b6aaebac5d
Anika Luo <al8@wellesley.edu>
2022-06-22 15:41:02 -0400

    outline

--- presentation/tutorial.jl
+++ presentation/tutorial.jl
@@ -5,7 +5,33 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ bd9652fc-f262-11ec-3cf6-71de5b26f0f0
+md"""
+# Microbiome.jl tutorial
+"""
 
+# ╔═╡ 8d01fe33-6610-44b1-800a-5fba9224359c
+md"""
+## Download microbial community data
+"""
+
+# ╔═╡ f5decd43-03ca-40ce-89a0-2cb74fa0fa70
+md"""
+## Download metadata
+"""
+
+# ╔═╡ c4df70fd-ea07-4601-846a-b492baed4d60
+md"""
+## Load data and metadata into Microbiome.jl
+"""
+
+# ╔═╡ 42ff9700-8d33-41b0-915a-acb6202bc227
+md"""
+## Create Principal Coordinate Analysis (PCoA)
+"""
 
 # ╔═╡ Cell order:
-# ╠═bd9652fc-f262-11ec-3cf6-71de5b26f0f0
+# ╟─bd9652fc-f262-11ec-3cf6-71de5b26f0f0
+# ╟─8d01fe33-6610-44b1-800a-5fba9224359c
+# ╟─f5decd43-03ca-40ce-89a0-2cb74fa0fa70
+# ╟─c4df70fd-ea07-4601-846a-b492baed4d60
+# ╟─42ff9700-8d33-41b0-915a-acb6202bc227
