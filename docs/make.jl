444d7a9112a5ab1406020737fd77859e3370ee21
Kevin Bonham <dev@bonham.ch>
2026-07-16 09:55:27 -0400

    Update to documenter v1

--- docs/make.jl
+++ docs/make.jl
@@ -2,19 +2,20 @@ using Documenter, Microbiome, Microbiome.Dictionaries
 
 makedocs(
     sitename = "Microbiome.jl",
-    strict=:doctest,
+    warnonly = [:missing_docs],
     pages = [
         "Home" => "index.md",
         "Samples and features" => "samples_features.md",
         "Profiles and Communities" => "profiles.md",
         "Diversity measures" => "diversity.md",
     ],
-    authors = "Kevin Bonham, PhD <kbonham@wellesley.edu",
+    authors = "Kevin Bonham, PhD <kbonham@wellesley.edu>",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
-        edit_link="main",
-        canonical="http://docs.ecojulia.org/BiobakeryUtils.jl/stable/")
-        )
+        edit_link = "main",
+        canonical = "https://docs.ecojulia.org/Microbiome.jl/stable/",
+    ),
+)
 
 deploydocs(
     repo = "github.com/EcoJulia/Microbiome.jl.git",
